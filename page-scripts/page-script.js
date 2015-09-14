20f4fa8fd825c2b76d224e6fb09cb7ba14f6b941
Will Bamberg <wbamberg@mozilla.com>
2015-09-14 11:39:25 -0700

    updated page script to add global var

--- page-scripts/page-script.js
+++ page-scripts/page-script.js
@@ -1,3 +1,5 @@
 var p = document.createElement("p");
-p.textContent = "And here's a paragraph, added by a page script";
+p.textContent = "Here's another paragraph, added by a page script.";
 document.body.appendChild(p);
+
+window.foo = "Here's a global variable, added by a page script";

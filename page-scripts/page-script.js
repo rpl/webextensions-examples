ce461266c6f70126d1b50fb35b2af27bdb65b26c
Will Bamberg <wbamberg@mozilla.com>
2015-09-14 13:20:43 -0700

    redefine confirm on page script

--- page-scripts/page-script.js
+++ page-scripts/page-script.js
@@ -3,3 +3,8 @@ p.textContent = "Here's another paragraph, added by a page script.";
 document.body.appendChild(p);
 
 window.foo = "Here's a global variable, added by a page script";
+
+window.confirm = function() {
+  console.log("The page script has also redefined 'confirm'");
+  return true;
+}

16284d1dc3099291005ecf726dbeb1ad55c4c74d
Will Bamberg <wbamberg@mozilla.com>
2015-09-14 14:20:37 -0700

    added some code behind the buttons

--- page-scripts/page-script.js
+++ page-scripts/page-script.js
@@ -1,9 +1,35 @@
 var p = document.createElement("p");
 p.textContent = "This paragraph was added by a page script.";
+p.setAttribute("id", "page-script-para");
 document.body.appendChild(p);
 
 window.foo = "This global variable was added by a page script";
 
 window.confirm = function() {
   return "The page script has also redefined 'confirm'";
+}
+
+// UI stuff
+
+var output = document.getElementById("output");
+
+var highlightPara = document.getElementById("highlight-para");
+var showFoo = document.getElementById("show-foo");
+var showWindowConfirm = document.getElementById("show-window.confirm");
+
+highlightPara.addEventListener("click", handleHighlightPara, false);
+showFoo.addEventListener("click", handleShowFoo, false);
+showWindowConfirm.addEventListener("click", handleShowWindowConfirm, false);
+
+function handleHighlightPara() {
+  var pageScriptPara = document.getElementById("page-script-para");
+  pageScriptPara.style.backgroundColor = "red";
+}
+
+function handleShowFoo() {
+
+}
+
+function handleShowWindowConfirm() {
+
 }

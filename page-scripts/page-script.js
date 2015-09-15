36456d6ae616afdee211895d3635aa7240bb4a1f
Will Bamberg <wbamberg@mozilla.com>
2015-09-14 17:35:46 -0700

    change confirm redefiniction

--- page-scripts/page-script.js
+++ page-scripts/page-script.js
@@ -6,7 +6,7 @@ document.body.appendChild(p);
 window.foo = "This global variable was added by a page script";
 
 window.confirm = function() {
-  return "The page script has also redefined 'confirm'";
+  alert("The page script has also redefined 'confirm'");
 }
 
 // UI stuff

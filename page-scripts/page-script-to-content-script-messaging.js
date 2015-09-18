bbf771ec8a8be1fb41eb2d3c6b906fac738cff5e
Will Bamberg <wbamberg@mozilla.com>
2015-09-17 22:16:46 -0700

    2-way messaging

--- page-scripts/page-script-to-content-script-messaging.js
+++ page-scripts/page-script-to-content-script-messaging.js
@@ -1,9 +1,18 @@
-var messenger = document.getElementById("messenger");
+var messenger = document.getElementById("from-page-script");
 
 messenger.addEventListener("click", sendMessage);
 
-function sendMessage() {
+function messageContentScript() {
   window.postMessage({
+    direction: "from-page-script",
     message: "Message from the page"
   }, "*");
 }
+
+window.addEventListener("message", function(event) {
+  if (event.source == window &&
+      event.data.direction &&
+      event.data.direction == "from-content-script") {
+    alert("Page script received message: \"" + event.data.message + "\"");
+  }
+});

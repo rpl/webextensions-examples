992ad4a9aabe626c5c709f9d85ecd2e6ee021bbf
Luca Greco <lgreco@mozilla.com>
2017-03-10 15:46:21 +0100

    TMP: additional tweaks on the example

--- back-to-sourcetab/background.js
+++ back-to-sourcetab/background.js
@@ -9,24 +9,31 @@ browser.webNavigation.onCreatedNavigationTarget.addListener(msg => {
 });
 
 
-function updatePageAction({tabId}) {
+function updatePageAction(event, tab) {
+  const {tabId} = tab || {};
+  console.log("updatePageAction on ", {event, tab, hasTabId: tabsOpener.has(tabId)});
   if (tabsOpener.has(tabId)) {
     browser.pageAction.show(tabId);
   } else {
     browser.pageAction.hide(tabId);
   }
 }
-browser.webNavigation.onCompleted.addListener(updatePageAction);
-browser.webNavigation.onErrorOccurred.addListener(updatePageAction);
-browser.webNavigation.onReferenceFragmentUpdated.addListener(updatePageAction);
-browser.webNavigation.onHistoryStateUpdated.addListener(updatePageAction);
-browser.tabs.onActivated.addListener(updatePageAction);
+browser.webNavigation.onCompleted.addListener(updatePageAction.bind(null, "onCompleted"));
+browser.webNavigation.onErrorOccurred.addListener(updatePageAction.bind(null, "onErrorOccurred"));
+browser.webNavigation.onReferenceFragmentUpdated.addListener(updatePageAction.bind(null, "onReferenceFragmentUpdated"));
+browser.webNavigation.onHistoryStateUpdated.addListener(updatePageAction.bind(null, "onHistoryStateUpdated"));
+browser.tabs.onActivated.addListener(updatePageAction.bind(null, "onActivated"));
 
 browser.tabs.onRemoved.addListener((tabId) => {
   tabsOpener.delete(tabId);
 });
 
 browser.pageAction.onClicked.addListener(async tab => {
+  console.log("PAGE ACTION CLICKED", tab);
+  // NOTE: workaround missing tab in android pageAction onClicked event.
+  if (!tab) {
+    tab = (await browser.tabs.query({active: true, currentWindow: true}))[0];
+  }
   const opener = tabsOpener.get(tab.id);
 
   if (!opener) {
